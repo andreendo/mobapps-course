ff8cd6731d6b6e7b92517b69d4aa965c8870f490
Andre T. Endo <andreendo@gmail.com>
2015-02-26 11:38:31 -0300

    MyLittleRandom option interval ok

--- MyLittleRandom/src/com/github/andreendo/mobappscourse/mylittlerandom/ManualListActivity.java
+++ MyLittleRandom/src/com/github/andreendo/mobappscourse/mylittlerandom/ManualListActivity.java
@@ -9,5 +9,7 @@ public class ManualListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_manual_list);
+		
+		//TODO
 	}
 }
