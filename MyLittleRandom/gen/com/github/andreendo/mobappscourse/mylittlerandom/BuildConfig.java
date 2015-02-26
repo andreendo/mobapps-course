de754d2bbd54d7b952078490bede009e71169faa
Andre T. Endo <andreendo@gmail.com>
2015-02-26 11:30:12 -0300

    untracking some folders

--- MyLittleRandom/gen/com/github/andreendo/mobappscourse/mylittlerandom/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.github.andreendo.mobappscourse.mylittlerandom;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
